8e7c4e6637e082d653d19991f4b10f9b6133d05a
Skygenak <fikimiki59@gmail.com>
2023-03-10 12:24:31 +0100

    commit kategorie gier componenty

--- app.module.ts
+++ app.module.ts
@@ -4,11 +4,31 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AkcjaComponent } from './akcja/akcja.component';
+import { SymulatoryComponent } from './symulatory/symulatory.component';
+import { SportoweComponent } from './sportowe/sportowe.component';
+import { StrategiczneTuroweComponent } from './strategiczne-turowe/strategiczne-turowe.component';
+import { StrategiczneRTSComponent } from './strategiczne-rts/strategiczne-rts.component';
+import { HorrorComponent } from './horror/horror.component';
+import { PrzygodoweComponent } from './przygodowe/przygodowe.component';
+import { WyscigoweComponent } from './wyscigowe/wyscigowe.component';
+import { SoulsLikeComponent } from './souls-like/souls-like.component';
+import { RougeLikeComponent } from './rouge-like/rouge-like.component';
+import { SurvivalComponent } from './survival/survival.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    AkcjaComponent
+    AkcjaComponent,
+    SymulatoryComponent,
+    SportoweComponent,
+    StrategiczneTuroweComponent,
+    StrategiczneRTSComponent,
+    HorrorComponent,
+    PrzygodoweComponent,
+    WyscigoweComponent,
+    SoulsLikeComponent,
+    RougeLikeComponent,
+    SurvivalComponent
   ],
   imports: [
     BrowserModule,
